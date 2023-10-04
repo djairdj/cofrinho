e260ff1d5ea5ac38e365067f9202ba5db097ea20
Djair Alves <djair.dj@gmail.com>
2023-10-04 10:43:10 -0300

    Inclusão da moeda Bitcoin nas opções do Cofre

--- src/cofrinho/MoedaTipo.java
+++ src/cofrinho/MoedaTipo.java
@@ -1,27 +1,35 @@
 package cofrinho;
 
+import cofrinho.api.Exchange;
+
 import java.util.Locale;
 
 public enum MoedaTipo {
-    REAL(1.0, new Locale("pt","BR")),
-    DOLLAR(2.0, new Locale("en","US")),
-    EURO(3.0, new Locale(Locale.ITALIAN.getCountry(), "IT")),
-    LIBRAS_ESTERLINAS(5.0, new Locale(Locale.UK.getLanguage(), Locale.UK.getCountry()));
+  REAL(EnumCoins.BRL, new Locale("pt", "BR")),
+  DOLLAR(EnumCoins.USD, new Locale("en", "US")),
+  EURO(EnumCoins.EUR, new Locale(Locale.ITALIAN.getCountry(), "IT")),
+  LIBRAS_ESTERLINAS(EnumCoins.GBP, new Locale(Locale.UK.getLanguage(), Locale.UK.getCountry())),
+  BITCOIN(EnumCoins.BTC, new Locale(Locale.ROOT.getLanguage()));
 
-    private double cambio;
-    private Locale local;
+  private double cambio;
+  private Locale local;
+  private EnumCoins sigla;
 
-    private MoedaTipo(double cambio, Locale local) {
-        this.cambio = cambio;
-        this.local= local;
-    }
+  private MoedaTipo(EnumCoins sigla, Locale local) {
+    this.sigla = sigla;
+    this.cambio = Exchange.getRate(sigla, EnumCoins.BRL);
+    this.local = local;
+  }
 
-    public double cambio() {
-        return this.cambio;
-    }
+  public double cambio() {
+    return this.cambio;
+  }
 
-    public void setCambio(double cambio) {
-        this.cambio = Math.abs(cambio);
-    }
-    public Locale local(){return this.local;}
+  public void setCambio(double cambio) {
+    this.cambio = Math.abs(cambio);
+  }
+
+  public Locale local() {
+    return this.local;
+  }
 }
