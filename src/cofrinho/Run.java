4f1dab654d804564660d21918a50290446b78110
Djair Alves <djair.dj@gmail.com>
2023-10-05 18:36:58 -0300

    Ajuste no TODO

--- src/cofrinho/Run.java
+++ src/cofrinho/Run.java
@@ -9,9 +9,11 @@ de moedas" em Java. Crie um menu em que é oferecido ao usuário:
  Calcular quanto dinheiro existe no cofrinho convertido para Real
  */
 /*
-TODO  [] Desmembrar a responsabilidade do valora taxa de câmbio da classe da moeda;
+TODO    [] Desmembrar a responsabilidade do valor da taxa de câmbio da classe da moeda;
         [] Criar um conversor para ser utilizado apenas na hora de checar os valores cambiados,
         dessa forma o cálculo será atualizado em tempo "real";
+        [] Gerar lógica de consumo da API e/ou do arquivo json de maneira dinâmica, tentando
+         consumir a API primeiro e caso dê errado ou time-out, consumir o arquivo
 */
 public class Run {
   public static void main(String[] args) {
