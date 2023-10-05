3ad061135fed4b7d59747de78208e7d233fb6890
Djair Alves <djair.dj@gmail.com>
2023-10-05 15:39:32 -0300

    Ajuste identação

--- src/cofrinho/Moeda.java
+++ src/cofrinho/Moeda.java
@@ -1,15 +1,15 @@
 package cofrinho;
 
 public class Moeda {
-    MoedaTipo tipo;
-    double valor;
+  MoedaTipo tipo;
+  double valor;
 
-    public Moeda(double value, MoedaTipo tipoMoeda) {
-        this.tipo = tipoMoeda;
-        this.valor = value;
-    }
+  public Moeda(double value, MoedaTipo tipoMoeda) {
+    this.tipo = tipoMoeda;
+    this.valor = value;
+  }
 
-    public String toString() {
-        return "Moeda de " + this.tipo + ", valor: " + this.valor + " -> Câmbio: " + this.tipo.cambio();
-    }
+  public String toString() {
+    return "Moeda de " + this.tipo + ", valor: " + this.valor + " -> Câmbio: " + this.tipo.cambio();
+  }
 }
